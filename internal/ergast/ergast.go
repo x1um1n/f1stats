22fe89e36a6eae4cc3ff6e343f818fb42f426557
matt-beaney <matt.beaney@autotrader.co.uk>
2019-12-23 11:59:42 +0000

    gets years active for all title-winning constructors

--- internal/ergast/ergast.go
+++ internal/ergast/ergast.go
@@ -19,7 +19,7 @@ type Constructor struct {
 	URL                string   `json:"url"`
 	Name               string   `json:"name"`
 	Nationality        string   `json:"nationality"`
-	ConTitleCount			 int			`json:"constructors-titles-count"`
+	ConTitleCount      int      `json:"constructors-titles-count"`
 	ConstructorsTitles []string `json:"constructors-titles"`
 	YearsActive        []string `json:"years-active"`       //fixme
 	YearsActiveH       string   `json:"years-active-human"` //fixme
@@ -34,11 +34,11 @@ type StandList struct {
 	Year string `json:"season"`
 }
 
-// GetChampConstructors gets all the Constructors who have won the constructors
+// getChampConstructors gets all the Constructors who have won the constructors
 // championship, the default limit is 30 and current count of unique Constructors
 // is 17, so there should be no need to either get a second page or increase the
 // results limit for the foreseeable future
-func GetChampConstructors() []Constructor {
+func getChampConstructors() []Constructor {
 	log.Println("Getting all championship-winning constructors")
 	response, err := http.Get("https://ergast.com/api/f1/constructorStandings/1/constructors.json")
 	if !checkerr.Check(err, "Failed to get all championship-winning constructors") {
@@ -59,10 +59,10 @@ func GetChampConstructors() []Constructor {
 	return nil
 }
 
-// GetConstructorsTitles gets all the years a constructor won the contructors
+// getConstructorsTitles gets all the years a constructor won the contructors
 // championship.  as with getChampConstructors, there is no constructor which
 // has won the title anywhere near 30 times
-func GetConstructorsTitles(con string) (titles []string) {
+func getConstructorsTitles(con string) (titles []string) {
 	log.Printf("Getting all constructors titles for %s\n", con)
 	response, err := http.Get("https://ergast.com/api/f1/constructors/" + con + "/constructorStandings/1.json")
 	if !checkerr.Check(err, "Failed to get all constructors titles for ", con) {
@@ -87,8 +87,8 @@ func GetConstructorsTitles(con string) (titles []string) {
 	return nil
 }
 
-// GetRaceStarts gets the total number of race starts for a constructor
-func GetRaceStarts(con string) int {
+// getRaceStarts gets the total number of race starts for a constructor
+func getRaceStarts(con string) int {
 	log.Printf("Getting all race starts for %s\n", con)
 	response, err := http.Get("https://ergast.com/api/f1/constructors/" + con + "/results.json?limit=0")
 	if !checkerr.Check(err, "Failed to get all race starts for ", con) {
@@ -108,8 +108,8 @@ func GetRaceStarts(con string) int {
 	return 0
 }
 
-// GetRaceWins gets the total number of race wins for a contructor
-func GetRaceWins(con string) int {
+// getRaceWins gets the total number of race wins for a contructor
+func getRaceWins(con string) int {
 	log.Printf("Getting all race wins for %s\n", con)
 	response, err := http.Get("https://ergast.com/api/f1/constructors/" + con + "/results/1.json?limit=0")
 	if !checkerr.Check(err, "Failed to get all race wins for ", con) {
@@ -129,6 +129,63 @@ func GetRaceWins(con string) int {
 	return 0
 }
 
+// getActiveYears gets all the seasons a constructor was active, currently
+// limited to 70, will need increasing at some point
+func getActiveYears(con string) (result []string) {
+	log.Printf("Getting all seasons %s competed in\n", con)
+	response, err := http.Get("https://ergast.com/api/f1/constructors/" + con + "/constructorStandings.json?limit=70")
+	if !checkerr.Check(err, "Failed to get all seasons for ", con) {
+		data, _ := ioutil.ReadAll(response.Body)
+		var res struct {
+			MRData struct {
+				StandingsTable struct {
+					Years []StandList `json:"StandingsLists"`
+				} `json:"StandingsTable"`
+			} `json:"MRData"`
+		}
+		json.Unmarshal(data, &res)
+
+		for _, t := range res.MRData.StandingsTable.Years {
+			result = append(result, t.Year)
+		}
+	}
+	return
+}
+
+// getYearSpans takes a slice of strings, each containing a YYYY year and
+// converting it to a span of years eg 1991-2000
+func getYearSpans(years []string) string {
+	type span struct {
+		start string
+		end   string
+	}
+	var spans []span
+	spndx := 0
+
+	for i, y := range years {
+		if i == 0 {
+			spans = []span{{y, y}}
+			continue
+		}
+		st, _ := strconv.Atoi(y)
+		nd, _ := strconv.Atoi(years[i-1])
+		if st == nd+1 {
+			spans[spndx].end = y
+			continue
+		} else { //if the years aren't consecutive, start a new span
+			spndx++
+			spans = append(spans, span{y, y})
+			continue
+		}
+	}
+
+	s := spans[0].start + "-" + spans[0].end
+	for spndx = 1; spndx < len(spans); spndx++ {
+		s += ", " + spans[spndx].start + "-" + spans[spndx].end
+	}
+	return s
+}
+
 // Repopulate empties the redis cache and get fresh stats from ergast
 func Repopulate() error {
 	c := shared.P.Get()
@@ -138,15 +195,17 @@ func Repopulate() error {
 	_, err := c.Do("FLUSHALL")
 	if !checkerr.Check(err, "Error flushing redis, abandoning attempt to repopulate the data") {
 		var teams []Constructor
-		teams = GetChampConstructors()
+		teams = getChampConstructors()
 
 		for i, t := range teams {
-			teams[i].ConstructorsTitles = GetConstructorsTitles(t.ConstructorID)
+			teams[i].ConstructorsTitles = getConstructorsTitles(t.ConstructorID)
 			teams[i].ConTitleCount = len(teams[i].ConstructorsTitles)
-			teams[i].RaceStarts = GetRaceStarts(t.ConstructorID)
-			teams[i].RaceWins = GetRaceWins(t.ConstructorID)
+			teams[i].RaceStarts = getRaceStarts(t.ConstructorID)
+			teams[i].RaceWins = getRaceWins(t.ConstructorID)
 			teams[i].WinRate = float32(teams[i].RaceWins) / float32(teams[i].RaceStarts)
 			teams[i].WinRateH = fmt.Sprintf("%.2f%% (%d wins from %d starts)", (teams[i].WinRate * 100), teams[i].RaceWins, teams[i].RaceStarts)
+			teams[i].YearsActive = getActiveYears(t.ConstructorID)
+			teams[i].YearsActiveH = getYearSpans(teams[i].YearsActive)
 
 			json, e := json.Marshal(teams[i])
 			if !checkerr.Check(e, "Error marshalling json") {
